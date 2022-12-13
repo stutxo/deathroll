05240e4057477e71daa9f23b799e0ee51ef7f219
stum0 <stum0@stum0.com>
2022-12-13 00:50:21 +0000

    clean up code

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,29 +1,13 @@
-use axum::{extract::Path, response::IntoResponse, routing::get, Json, Router};
+use axum::{extract::Path, routing::get, Json, Router};
+use axum_extra::routing::SpaRouter;
 use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
-// use tower_http::cors::{Any, CorsLayer};
-use axum_extra::routing::SpaRouter;
-use tower::ServiceBuilder;
-use tower_http::trace::TraceLayer;
 
 #[tokio::main]
 async fn main() {
-    //let cors = CorsLayer::new().allow_origin(Any);
-
-    //let app = Router::new().route("/:id", get(get_id)).layer(cors);
-
-    // let app = Router::new()
-    //     .route("/egg", get(hello))
-    //     .merge(SpaRouter::new("/assets", "../dist"))
-    //     .layer(ServiceBuilder::new().layer(TraceLayer::new_for_http()));
-
     let spa = SpaRouter::new("/assets", "../dist");
 
-    let app = Router::new()
-        // `SpaRouter` implements `Into<Router>` so it works with `merge`
-        .merge(spa)
-        // we can still add other routes
-        .route("/api/foo", get(hello));
+    let app = Router::new().merge(spa).route("/game/:id", get(get_id));
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3030));
     println!("listening on {}", addr);
@@ -38,10 +22,6 @@ async fn get_id(Path(id): Path<String>) -> Json<Id> {
     let reply_id = Id { store_id: id };
     println!("{:?}", reply_id);
     Json(reply_id)
-}
-
-async fn hello() -> impl IntoResponse {
-    "hello from server!"
 }
 
 #[derive(Serialize, Deserialize, Debug)]
